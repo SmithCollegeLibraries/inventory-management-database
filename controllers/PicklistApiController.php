b2608b99124d32ab9fb9c26c922886762952a5fe
Micah Walter <micahjwalter@gmail.com>
2024-03-20 13:23:31 -0400

    Hotfix: picklist handle special characters

--- controllers/PicklistApiController.php
+++ controllers/PicklistApiController.php
@@ -15,7 +15,7 @@ class PicklistApiController extends ActiveController
 
     static function truncate($string, $length)
     {
-        return strlen($string) > $length ? substr($string, 0, $length-1).'…' : $string;
+        return strlen($string) > $length ? mb_substr($string, 0, $length-1).'…' : $string;
     }
 
     public function init()
