2f177e97432b6068f0aabd4aa1e801eb9f2b273e
Micah Walter <micahjwalter@gmail.com>
2025-02-05 00:12:58 -0500

    Revise searching by free space

--- controllers/ShelfApiController.php
+++ controllers/ShelfApiController.php
@@ -356,7 +356,7 @@ class ShelfApiController extends ActiveController
                 ]);
             }
             // If the user is looking for empty shelves specifically
-            else if ($positionsFree == -1) {
+            else if ($positionsFree === -1) {
                 $provider = new ActiveDataProvider([
                     'query' => $this->modelClass::find()
                         ->leftJoin('tray', 'tray.shelf_id = shelf.id')
@@ -376,7 +376,7 @@ class ShelfApiController extends ActiveController
                     ],
                 ]);
             }
-            else if ($positionsFree == 0) {
+            else if ($positionsFree === 0) {
                 $provider = new ActiveDataProvider([
                     'query' => $this->modelClass::find()
                         ->leftJoin('tray', 'tray.shelf_id = shelf.id')
@@ -397,7 +397,7 @@ class ShelfApiController extends ActiveController
                     ],
                 ]);
             }
-            else if ($positionsFree !== null && $positionsFree !== "") {
+            else if (!$positionsFree) {
                 $provider = new ActiveDataProvider([
                     'query' => $this->modelClass::find()
                         ->leftJoin('tray', 'tray.shelf_id = shelf.id')
